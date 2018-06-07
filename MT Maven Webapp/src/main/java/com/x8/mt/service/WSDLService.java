6b2abf5fae2814ab8b966e32f05b1046ac0340d2
919869387@qq.com <919869387@qq.com>
2018-06-07 14:15:15 +0800

    添加和为请求wsdl方法

--- MT Maven Webapp/src/main/java/com/x8/mt/service/WSDLService.java
+++ MT Maven Webapp/src/main/java/com/x8/mt/service/WSDLService.java
@@ -1,23 +1,29 @@
 package com.x8.mt.service;
 
+import java.rmi.RemoteException;
+
 import javax.annotation.Resource;
 import javax.xml.namespace.QName;
+import javax.xml.rpc.ParameterMode;
+import javax.xml.rpc.ServiceException;
 
 import net.sf.json.JSONObject;
 
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.apache.axis.encoding.XMLType;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.rpc.client.RPCServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Service;
 
 import com.x8.mt.common.GlobalMethodAndParams;
 import com.x8.mt.entity.Metadata;
 import com.x8.mt.entity.WsdlServiceInfo;
 
-@Service
+@org.springframework.stereotype.Service
 public class WSDLService {
 
 	@Autowired  
@@ -109,7 +115,8 @@ public class WSDLService {
 		threadPoolTaskExecutor.execute(new Runnable() {  
 			@Override  
 			public void run() {
-				sendWebService(url,tns,method,finalOperationType,finalProtocolType,finalProtocolName,finalProtocolId);
+				//sendWebService(url,tns,method,finalOperationType,finalProtocolType,finalProtocolName,finalProtocolId);
+				sendWebService_HEWEI(finalOperationType,finalProtocolType,finalProtocolName,finalProtocolId);//和为提供的方法
 			}  
 		}); 
 	}
@@ -146,5 +153,44 @@ public class WSDLService {
 		} catch (AxisFault e) {
 			System.out.println("webservice出错");
 		}
+	}
+	
+	/**
+	 * 
+	 * 作者:allen
+	 * 时间:2018年6月7日
+	 * 作用:和为提供-发送webservice
+	 */
+	String sendWebService_HEWEI(String operationType,String protocolType,String protocolName,String protocolId) {
+		String endpoint = "http://localhost:8080/webservice/protocolservice/ProtocolManagerService";
+		String result = "no result!";
+		Service service = new Service();
+		Call call;
+		Object object[] = {operationType,protocolType,protocolName,protocolId};
+		try {
+			call = (Call) service.createCall();
+			call.setTargetEndpointAddress(endpoint);// 远程调用路径
+			call.setOperationName("notifying");// 调用的方法名
+
+			// 设置参数名:
+			call.addParameter("operationType", // 参数名
+					XMLType.XSD_STRING,// 参数类型:String
+					ParameterMode.IN);// 参数模式：'IN' or 'OUT'
+			call.addParameter("protocolType", // 参数名
+					XMLType.XSD_STRING,// 参数类型:String
+					ParameterMode.IN);// 参数模式：'IN' or 'OUT'
+			call.addParameter("protocolName", // 参数名
+					XMLType.XSD_STRING,// 参数类型:String
+					ParameterMode.IN);// 参数模式：'IN' or 'OUT'
+			call.addParameter("protocolId", // 参数名
+					XMLType.XSD_STRING,// 参数类型:String
+					ParameterMode.IN);// 参数模式：'IN' or 'OUT'			
+			result = (String) call.invoke(object);// 远程调用
+		} catch (ServiceException e) {
+			System.out.println("wsdl服务error1");
+		} catch (RemoteException e) {
+			System.out.println("wsdl服务error2");
+		}
+		return result;
 	}
 }
